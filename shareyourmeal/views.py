8e77a7bbcb44e27d0e930897dc8eb03b35d55ac0
swathi-kannan <77623009+swathi-kannan@users.noreply.github.com>
2021-07-13 10:27:18 +0530

    Add files via upload

--- shareyourmeal/views.py
+++ shareyourmeal/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render , redirect
 from shareyourmeal.models import Userreg 
 from shareyourmeal.models import Pay
-from shareyourmeal.models import Addpost
+from shareyourmeal.models import home
+from shareyourmeal.forms import UserForm
 from django.contrib import messages
 import razorpay
 from django.views.decorators.csrf import csrf_exempt
@@ -26,22 +27,40 @@ def success(request):
 
 def aboutus(request):
     return render(request,'aboutus.html')
-def Post(request):
+def Homepage(request):
     if request.method == 'POST':
-        if request.POST.get('reason') and request.POST.get('count') and request.POST.get('phoneno') and request.POST.get('address') and request.POST.get('caption'):
-            saverecord=Addpost()
+        if request.POST.get('username') and request.POST.get('reason') and request.POST.get('count') and request.POST.get('phoneno') and request.POST.get('address') and request.POST.get('caption'):
+            saverecord=home()
+            saverecord.username=request.POST.get('username')
             saverecord.reason=request.POST.get('reason')
             saverecord.count=request.POST.get('count')
             saverecord.phoneno=request.POST.get('phoneno')
             saverecord.address=request.POST.get('address')
             saverecord.caption=request.POST.get('caption')
             saverecord.save()
-            messages.success(request,'Posted successfully')
-            return render(request,'socio.html')
-    else:
-        return render(request,'socio.html')
+            mypost = home.objects.filter(username=request.session['username'])
+            display= home.objects.all()
+            return render(request,'socio.html',{'display': home.objects.all(), 'home': mypost})
+        elif 'delete' in request.POST:
+            particular = home.objects.filter(username=request.session['username'])
+            particular.delete()
+            display= home.objects.all()
+            return render(request, 'socio.html', {'display': home.objects.all(),'home': particular})
+    mypost = home.objects.filter(username=request.session['username'])
+    display= home.objects.all()
+    return render(request,'socio.html',{'display': home.objects.all(), 'home': mypost})
 def profile(request):
-    return render(request,'profile.html')
+            ab = {'Userreg': Userreg.objects.filter(username=request.session['username'])}
+            if request.method == "POST":
+                username = request.POST['username']
+                email = request.POST['email']
+                phoneno = request.POST['phoneno']
+                address = request.POST['address']
+                s = Userreg.objects.filter(username=request.session['username']).update(username=username,email=email, phoneno=phoneno, address= address)
+                messages.success(request,'Updated successfully')
+                return render(request, 'profile.html', ab)
+            Userprofile=Userreg.objects.filter(username=request.session['username'])
+            return render(request, 'profile.html', {'Userreg': Userprofile}) 
 def Userregistration(request):
     if request.method == 'POST':
         if request.POST.get('username') and request.POST.get('email') and request.POST.get('phoneno') and request.POST.get('address') and request.POST.get('password') and request.POST.get('type'):
